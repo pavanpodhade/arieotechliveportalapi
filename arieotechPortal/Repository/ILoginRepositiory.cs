ca7a13db8931b3d3fe7e8ef4ed860656e59065c2
AajitShete <ajit.shete@arieotech.com>
2022-01-03 22:12:59 +0530

    check for duplicate adhar number in health card

--- arieotechPortal/Repository/ILoginRepositiory.cs
+++ arieotechPortal/Repository/ILoginRepositiory.cs
@@ -1,15 +1,4 @@
-﻿using ArieotechLive.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ArieotechLive.Model;
-using ArieotechLive.Repository;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.SqlServer.Management.Smo;
-using Octopus.Client.Repositories.Async;
-
+﻿
 using Login = ArieotechLive.Model.Login;
 
 namespace ArieotechLive.Repository
